8fedec4f31539e45c0029d16e1e7f464a318033b
GNU Gettext Build <bug-gnu-gettext@gnu.org>
2021-07-30 18:45:40 -0600

    mobile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,89 @@
 <template>
-  <div class="home page mt">
-    <div class="inline">
-      <h2 class="t2">Welcome to</h2>
-      <h1 class="title">The Reawaken</h1>
-      <h1 class="title">Foundation</h1>
-      <h3 class="t2">&rarr; We Serve Coffee With a Purpose.</h3>
-    </div>
-    <div class="parent inline">
-      <img
-        src="@/assets/cup-of-coffee.jpeg"
-        id="coffee-pic"
-        class="inline"
-        alt="Image unavailable"
-      />
+  <div class="wrapper">
+    <div v-if="mobileView" class="mobile">
       <div class="inline">
-        <table>
-          <tr>
-            <h4 class="p1">
-              At Reawaken we fill a basic need (literally a cup of coffee).
-              While serving you coffee, we are employing Traumatic Brain Injury
-              survivors. Why buy just by a cup of coffee when you could get the
-              same cup of coffee helping out people like our founder who was in
-              a coma due to his TBI? Read more about what TBI is and how we are
-              "helping"
-              <a href="/whatistbi">here</a>.
-            </h4>
-            <h4 class="p2">
-              Aside from just serving coffee at your company or at your events,
-              we also offer technical services to help give your business an
-              edge in our digital world. We will consult with your business on
-              what services you may need and how to effectively use media to
-              grow and keep your business on the cutting edge. Aside from just
-              consulting, we offer video and we design services to help produce
-              thi cutting edge media for your business.
-            </h4>
-            <h4 class="p2">
-              One of our aims is to help support other nonprofits and to open up
-              our very own coffee shop where TBI survivors and other individuals
-              with disabilities can hang out and better understand their
-              disabilities. When you purchase a cup of coffee at an event where
-              our foundation is working, money that we make goes to our fund
-              where we help provide funds to pay for TBI survivors' medical debt
-              and other expenses incurred as a result of getting a TBI.
-              Rebounding from getting a TBI is nothing simple. It often takes
-              multiple years for survivors to fully recover.
-            </h4>
-          </tr>
-        </table>
+        <h2 class="title mobileT">The Reawaken</h2>
+        <h2 class="title mobileT">Foundation</h2>
+      </div>
+      <div class="page text">
+        At Reawaken we fill a basic need (literally a cup of coffee). While
+        serving you coffee, we are employing Traumatic Brain Injury survivors.
+        Why buy just by a cup of coffee when you could get the same cup of
+        coffee helping out people like our founder who was in a coma due to his
+        TBI? Read more about what TBI is and how we are "helping"
+        <a href="/whatistbi">here</a>.
+        <br />
+        Aside from just serving coffee at your company or at your events, we
+        also offer technical services to help give your business an edge in our
+        digital world. We will consult with your business on what services you
+        may need and how to effectively use media to grow and keep your business
+        on the cutting edge. Aside from just consulting, we offer video and we
+        design services to help produce thi cutting edge media for your
+        business.
+        <br />
+        One of our aims is to help support other nonprofits and to open up our
+        very own coffee shop where TBI survivors and other individuals with
+        disabilities can hang out and better understand their disabilities. When
+        you purchase a cup of coffee at an event where our foundation is
+        working, money that we make goes to our fund where we help provide funds
+        to pay for TBI survivors' medical debt and other expenses incurred as a
+        result of getting a TBI. Rebounding from getting a TBI is nothing
+        simple. It often takes multiple years for survivors to fully recover.
+      </div>
+    </div>
+    <div v-if="!mobileView" class="normal">
+      <div class="home page mt">
+        <div class="inline">
+          <h2 class="t2">Welcome to</h2>
+          <h1 class="title">The Reawaken</h1>
+          <h1 class="title">Foundation</h1>
+          <h3 class="t2">&rarr; We Serve Coffee With a Purpose.</h3>
+        </div>
+        <div class="parent inline">
+          <img
+            src="@/assets/cup-of-coffee.jpeg"
+            id="coffee-pic"
+            class="inline"
+            alt="Image unavailable"
+          />
+          <div class="inline">
+            <table>
+              <tr>
+                <h4 class="p1">
+                  At Reawaken we fill a basic need (literally a cup of coffee).
+                  While serving you coffee, we are employing Traumatic Brain
+                  Injury survivors. Why buy just by a cup of coffee when you
+                  could get the same cup of coffee helping out people like our
+                  founder who was in a coma due to his TBI? Read more about what
+                  TBI is and how we are "helping"
+                  <a href="/whatistbi">here</a>.
+                </h4>
+                <h4 class="p2">
+                  Aside from just serving coffee at your company or at your
+                  events, we also offer technical services to help give your
+                  business an edge in our digital world. We will consult with
+                  your business on what services you may need and how to
+                  effectively use media to grow and keep your business on the
+                  cutting edge. Aside from just consulting, we offer video and
+                  we design services to help produce thi cutting edge media for
+                  your business.
+                </h4>
+                <h4 class="p2">
+                  One of our aims is to help support other nonprofits and to
+                  open up our very own coffee shop where TBI survivors and other
+                  individuals with disabilities can hang out and better
+                  understand their disabilities. When you purchase a cup of
+                  coffee at an event where our foundation is working, money that
+                  we make goes to our fund where we help provide funds to pay
+                  for TBI survivors' medical debt and other expenses incurred as
+                  a result of getting a TBI. Rebounding from getting a TBI is
+                  nothing simple. It often takes multiple years for survivors to
+                  fully recover.
+                </h4>
+              </tr>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -55,7 +92,19 @@
 <script>
 export default {
   name: "Home",
+  data() {
+    return { mobileView: false };
+  },
   components: {},
+  methods: {
+    mobile() {
+      this.mobileView = window.innerWidth <= 990;
+      console.log("mobile");
+    },
+  },
+  created() {
+    this.mobile();
+  },
 };
 </script>
 
@@ -71,5 +120,13 @@ export default {
 }
 .mt {
   margin-top: 1%;
+}
+.mobileT {
+  margin-top: 1%;
+  margin-right: 1%;
+}
+.page {
+  height: fit-content;
+  padding: 3%;
 }
 </style>
--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,61 +1,124 @@
 <template>
-  <div class="services page">
-    <h1 class="t1">Services</h1>
-    <!-- <img id="media" src="@/assets/media.png" alt="Pic unavailable" /> -->
-    <table>
-      <tr>
-        <th>Web Design</th>
-        <th>Content Creation</th>
-        <th>Technical/Business Consulting</th>
-      </tr>
-      <tr>
-        <td>Word Press Website Design and template picking/editing.</td>
-        <td>
-          Creation, editing, and advising in regards to adding web content to
-          you or your business's website.
-        </td>
-        <td>
-          Consultation to help plan your company's digital media and digital
-          presence.
-        </td>
-      </tr>
-      <tr>
-        <td>
-          Do you need help creating a website? In this day and age it is
-          absolutely necessary to build a website and have content where
-          clients/users can get involved with and learn about what services you
-          offer.
-        </td>
-        <td>
-          Lets say you already have a website, you likely need to update your
-          website with relevant content that matches the aesthetic style that
-          yiou want to deliver to customers and slients of your business.
-        </td>
-        <td>
-          How do you want your business to be seen and observed by others? We
-          will help with consulting in regards to what technology or other
-          services you might require in order to serve your customers/clients
-          effectively.
-        </td>
-      </tr>
-      <tr>
-        <td>Updating or editing of non Wordpress websites.</td>
-        <td>Video production/editing</td>
-        <td>
-          Technical consulting in regards to setting up your business. WIFI
-          planning & setup./ Set up, use and storage of video equipment.
-        </td>
-      </tr>
-    </table>
+  <div>
+    <div v-if="mobileView">
+      <table class="mobileT">
+        <tr>
+          <th>Web Design</th>
+          <th>Content Creation</th>
+          <th>Technical/Business Consulting</th>
+        </tr>
+        <tr>
+          <td>Word Press Website Design and template picking/editing.</td>
+          <td>
+            Creation, editing, and advising in regards to adding web content to
+            you or your business's website.
+          </td>
+          <td>
+            Consultation to help plan your company's digital media and digital
+            presence.
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Do you need help creating a website? In this day and age it is
+            absolutely necessary to build a website and have content where
+            clients/users can get involved with and learn about what services
+            you offer.
+          </td>
+          <td>
+            Lets say you already have a website, you likely need to update your
+            website with relevant content that matches the aesthetic style that
+            yiou want to deliver to customers and slients of your business.
+          </td>
+          <td>
+            How do you want your business to be seen and observed by others? We
+            will help with consulting in regards to what technology or other
+            services you might require in order to serve your customers/clients
+            effectively.
+          </td>
+        </tr>
+        <tr>
+          <td>Updating or editing of non Wordpress websites.</td>
+          <td>Video production/editing</td>
+          <td>
+            Technical consulting in regards to setting up your business. WIFI
+            planning & setup./ Set up, use and storage of video equipment.
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div class="services page" v-if="!mobileView">
+      <h1 class="t1">Services</h1>
+      <!-- <img id="media" src="@/assets/media.png" alt="Pic unavailable" /> -->
+      <table class="reg">
+        <tr>
+          <th>Web Design</th>
+          <th>Content Creation</th>
+          <th>Technical/Business Consulting</th>
+        </tr>
+        <tr>
+          <td>Word Press Website Design and template picking/editing.</td>
+          <td>
+            Creation, editing, and advising in regards to adding web content to
+            you or your business's website.
+          </td>
+          <td>
+            Consultation to help plan your company's digital media and digital
+            presence.
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Do you need help creating a website? In this day and age it is
+            absolutely necessary to build a website and have content where
+            clients/users can get involved with and learn about what services
+            you offer.
+          </td>
+          <td>
+            Lets say you already have a website, you likely need to update your
+            website with relevant content that matches the aesthetic style that
+            yiou want to deliver to customers and slients of your business.
+          </td>
+          <td>
+            How do you want your business to be seen and observed by others? We
+            will help with consulting in regards to what technology or other
+            services you might require in order to serve your customers/clients
+            effectively.
+          </td>
+        </tr>
+        <tr>
+          <td>Updating or editing of non Wordpress websites.</td>
+          <td>Video production/editing</td>
+          <td>
+            Technical consulting in regards to setting up your business. WIFI
+            planning & setup./ Set up, use and storage of video equipment.
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return { mobileView: false };
+  },
+  components: {},
+  methods: {
+    mobile() {
+      this.mobileView = window.innerWidth <= 990;
+      console.log("mobile");
+    },
+  },
+  created() {
+    this.mobile();
+  },
+};
 </script>
 
 <style scoped>
-table {
+.reg {
   margin-left: auto;
   margin-right: auto;
   font-family: Arial, Helvetica, sans-serif;
@@ -63,21 +126,21 @@ table {
   width: 100%;
 }
 
-table td,
-table th {
+.reg td,
+.reg th {
   border: 1px solid #ddd;
   padding: 8px;
 }
 
-table tr:nth-child(even) {
+.reg tr:nth-child(even) {
   background-color: #f2f2f2;
 }
 
-table td:hover {
+.reg td:hover {
   background-color: #ddd;
 }
 
-table th {
+.reg th {
   padding-top: 12px;
   padding-bottom: 12px;
   text-align: left;
@@ -86,5 +149,9 @@ table th {
 }
 #media {
   width: 25%;
+}
+.mobileT {
+  background: white;
+  padding: 3%;
 }
 </style>
