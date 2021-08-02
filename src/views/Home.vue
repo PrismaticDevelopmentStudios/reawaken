30b7c112627c16567c52a2c6630e24bff82b25d8
GNU Gettext Build <bug-gnu-gettext@gnu.org>
2021-08-02 13:46:39 -0600

    8/2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,31 +4,45 @@
       <div class="inline">
         <h1 class="title mobileT">The Reawaken Foundation</h1>
       </div>
-      <!-- <img src="@/assets/coffee.jpg" id="coffee-pic" alt="" class="inline" /> -->
       <div class="page text">
-        At Reawaken we fill a basic need (literally a cup of coffee). While
-        serving you coffee, we are employing Traumatic Brain Injury survivors.
-        Why buy just by a cup of coffee when you could get the same cup of
-        coffee helping out people like our founder who was in a coma due to his
-        TBI? Read more about what TBI is and how we are helping on the "What Is
-        TBI" page.
+        <div class="inline">
+          <img
+            src="@/assets/cup-of-coffee.jpeg"
+            id="coffee-pic"
+            alt=""
+            class="mobile-coffee"
+          />
+          <div class="p1">
+            At Reawaken we fill a basic need (literally a cup of coffee). While
+            serving you coffee, we are employing Traumatic Brain Injury
+            survivors. Why buy just by a cup of coffee when you could get the
+            same cup of coffee helping out people like our founder who was in a
+            coma due to his TBI? Read more about what TBI is and how we are
+            helping on the "What Is TBI" page.
+          </div>
+        </div>
         <br />
-        Aside from just serving coffee at your company or at your events, we
-        also offer technical services to help give your business an edge in our
-        digital world. We will consult with your business on what services you
-        may need and how to effectively use media to grow and keep your business
-        on the cutting edge. Aside from just consulting, we offer video and we
-        design services to help produce thi cutting edge media for your
-        business.
+        <div class="inline p2">
+          Aside from just serving coffee at your company or at your events, we
+          also offer technical services to help give your business an edge in
+          our digital world. We will consult with your business on what services
+          you may need and how to effectively use media to grow and keep your
+          business on the cutting edge. Aside from just consulting, we offer
+          video and we design services to help produce thi cutting edge media
+          for your business.
+        </div>
         <br />
-        One of our aims is to help support other nonprofits and to open up our
-        very own coffee shop where TBI survivors and other individuals with
-        disabilities can hang out and better understand their disabilities. When
-        you purchase a cup of coffee at an event where our foundation is
-        working, money that we make goes to our fund where we help provide funds
-        to pay for TBI survivors' medical debt and other expenses incurred as a
-        result of getting a TBI. Rebounding from getting a TBI is nothing
-        simple. It often takes multiple years for survivors to fully recover.
+        <div class="inline p2">
+          One of our aims is to help support other nonprofits and to open up our
+          very own coffee shop where TBI survivors and other individuals with
+          disabilities can hang out and better understand their disabilities.
+          When you purchase a cup of coffee at an event where our foundation is
+          working, money that we make goes to our fund where we help provide
+          funds to pay for TBI survivors' medical debt and other expenses
+          incurred as a result of getting a TBI. Rebounding from getting a TBI
+          is nothing simple. It often takes multiple years for survivors to
+          fully recover.
+        </div>
       </div>
     </div>
     <div v-if="!mobileView" class="normal">
@@ -40,7 +54,7 @@
         </div>
         <div class="parent inline">
           <img
-            src="@/assets/cup-of-coffee.jpeg"
+            src="@/assets/coffee.jpg"
             id="coffee-pic"
             class="inline"
             alt="Image unavailable"
--- src/views/WhatIsTBI.vue
+++ src/views/WhatIsTBI.vue
@@ -2,7 +2,46 @@
   <div class="page">
     <table>
       <tr>
-        <td>
+        <td v-if="mobileView">
+          <img
+            src="@/assets/brain.jpeg"
+            id="mobile-brain"
+            alt="Image Unavailable"
+          />
+          <h1 class="t1">What is TBI?</h1>
+          <div class="text">
+            TBI, or traumatic brain injury happens to far more people than you
+            likely realize. Most commonly be incurr a TBI from servere car
+            accidents, but also one can get a TBI from playing sports such as
+            footbal, mountain biking, motocross, riding horses, or simply
+            slipping in the shower. A TBI is considered an invisible injury
+            because sometimes TBI survivors may look uninjured, but because
+            one's brain controls your entire body, there can be complications
+            and issues with cognitiion or behavior.
+          </div>
+          <div class="text">
+            TBI is often very difficult to manage and often many survivors need
+            to be medically cleared in order to work and to drive. This can be
+            frustrating because government services are not always immediately
+            accessible. Surviving with a TBI is indeed very complicated and is
+            difficult for not just the survivors, but also their loved oines and
+            families.
+          </div>
+          <div class="text">
+            Reawaken is intended to help get survivors back on their feet again
+            by providing a safe space to begin working again. This can inckude
+            offering shorter shifts in order to prevent our survivors from
+            becoming to fatigued. There are often many different physical or
+            mental struggles that TBI survivors have to grapple with on a daily
+            basis. But simply buying a cup of coffee from us can help to begin
+            to make a difference in survivors lives, which is a big deal
+            considering that getting a TBI often means that these survivors
+            almost died.
+          </div>
+        </td>
+
+        <td v-if="!mobileView">
+          <img src="@/assets/brain.jpeg" id="brain" alt="Image Unavailable" />
           <h1 class="t1">What is TBI?</h1>
           <div class="text">
             TBI, or traumatic brain injury happens to far more people than you
@@ -36,12 +75,25 @@
         </td>
       </tr>
     </table>
-    <img src="@/assets/brain.jpeg" id="brain" alt="Image Unavailable" />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return { mobileView: false };
+  },
+  components: {},
+  methods: {
+    mobile() {
+      this.mobileView = window.innerWidth <= 990;
+      console.log("mobile");
+    },
+  },
+  created() {
+    this.mobile();
+  },
+};
 </script>
 
 <style coped>
@@ -53,7 +105,13 @@ export default {};
 }
 #brain {
   width: 25%;
-  margin-top: 5%;
+  margin: 5%;
+  float: left;
+}
+#mobile-brain {
+  width: 45%;
+  margin: 5%;
+  float: left;
 }
 .inline {
   display: inline-block;
